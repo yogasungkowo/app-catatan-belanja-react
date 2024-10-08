87e8e60f26fce5b3fe6eae82ec0e4fc5a44c7d85
Prayoga Sungkowo <prayogasungkowo12@gmail.com>
2024-10-08 17:22:17 +0700

    perubahan sikit

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,16 @@ const groceryList = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState(groceryList);
+
+  function handleAddItem(newItem) {
+    setItems([...items, newItem]);
+  }
   return (
     <div className="app">
       <Header />
-      <Form />  
+      <Form onAddItem={handleAddItem} />
+      <GroceryList items={items} />
       <Footer />
     </div>
   );
@@ -20,8 +26,6 @@ function Header() {
   return <h1>Catatan Belanjaku 📝</h1>;
 }
 
-
-
 function Footer() {
   return (
     <footer className="stats">
@@ -30,35 +34,67 @@ function Footer() {
   );
 }
 
-function Form() {
+function Form({ onAddItem }) {
   const [name, setName] = useState("");
   const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
-    alert(quantity);    
+
+    if (!name) {
+      alert("Mohon di isi dulu barangnya");
+      return;
+    }
+
+    const newItem = {
+      name: name,
+      quantity: quantity,
+      checked: false,
+      id: Date.now(),
+    };
+    onAddItem(newItem);
+    console.log(newItem);
+    setName("");
+    setQuantity(1);
   }
 
-  const quantityNum = [...Array(20)].map((_, index) => (
-    <option value="{index + 1}" key={index}>{index + 1}</option>
+  const quantityNum = [...Array(20)].map((_, i) => (
+    <option value={i + 1} key={i}>
+      {i + 1}
+    </option>
   ));
   return (
     <>
       <form className="add-form" onSubmit={handleSubmit}>
         <h3>Hari ini belanja apa kita?</h3>
         <div>
-          <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+          <select
+            value={quantity}
+            onChange={(e) => setQuantity(Number(e.target.value))}
+          >
             {quantityNum}
           </select>
-          <input type="text" placeholder="nama barang..." value={name} onChange={(e) => setName(e.target.value)} />
+          <input
+            type="text"
+            placeholder="nama barang..."
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <button>Tambah</button>
       </form>
+    </>
+  );
+}
+
+function GroceryList({items}) {
+  return (
+    <>
       <div className="list">
         <ul>
-          {groceryList.map((item) => (
+          {items.map((item) => (
             <li key={item.id}>
-              <input type="checkbox" checked={item.checked} />
+              <input type="checkbox" checked={item.checked} onChange={() => toggleChecked(item.id)} />
               <span
                 style={{
                   textDecoration: item.checked ? "line-through" : "none",
